58f2af962bc8ae70941ed99c3839b9548bfe8e70
Isaiah Weaver <iswea1@morgan.edu>
2016-05-08 02:34:29 -0400

    added class Robot

---
+++ src/MethodParameters.java
@@ -0,0 +1,15 @@
+class Robot{
+	public void speak(){
+		System.out.println("Hello");
+	}
+}
+public class MethodParameters {
+
+	public static void main(String[] args) {
+		Robot isa = new Robot();
+		isa.speak();
+		
+
+	}
+
+}
